44921dad21965f727dc7505cbb4ea0f37825dfe0
pherna06 <pablohernandezaguado@gmail.com>
2020-11-08 21:26:37 +0100

    Added missing 'gym-cpu' local Gym module setup file.

--- gym_envs/cpuEnv_test.py
+++ gym_envs/cpuEnv_test.py
@@ -1,5 +1,5 @@
 import gym
-import gym-cpu
+import gym_cpu
 
 def run_test(env, verbose=False):
     env.reset()
---
+++ gym_envs/gym-cpu/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(
+    name='gym-cpu',
+    version='1.0.0',
+    install_requires = ['gym', 'cpufreq', 'pyRAPL']
+)
