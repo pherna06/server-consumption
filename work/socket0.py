1cee8d764abcc4cc5e5a320db360dc0c06dad18b
pherna06 <pablohernandezaguado@gmail.com>
2020-11-10 00:51:30 +0100

    Replaced C file for Python script.

---
+++ work/socket0.py
@@ -0,0 +1,21 @@
+import os
+import signal
+
+def process(core):
+    command = "taskset -c " + str(core) + " "
+    command += "/home/pherna06/venv-esfinge/server-consumption/work/test"
+
+    os.system(command)
+
+pid = []
+for i in range(8):
+    pid.append(os.fork())
+    if pid[i] == 0: #child
+        process(i)
+        exit()
+
+print("Press any key to kill processes.")
+input()
+
+for i in range(8):
+    os.kill(pid[i], signal.SIGKILL)
