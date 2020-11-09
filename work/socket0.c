ad3c2c99bcdb8a50cebe46bbf205bcb1caca2b20
pherna06 <pablohernandezaguado@gmail.com>
2020-11-10 00:11:19 +0100

    New work file, 'socket0', to start process in the 8 cores of CPU socket 0.

---
+++ work/socket0.c
@@ -0,0 +1,29 @@
+#include <unistd.h>
+#include <stdio.h>
+
+int main()
+{
+    pid_t pid[8];
+
+    for (int i = 0; i < 8; ++i)
+    {
+        pid[i] = fork()
+        if (pid[i] == 0)
+        {
+            // Child.
+            execl(
+                "/usr/bin/taskset",
+                "taskset", "-c", i,
+                "/home/pherna06/venv-esfinge/server-consumption/work/test")
+            break;
+        }        
+    }
+
+    std::printf("Press any key to kill processes.");
+    int c = getchar();
+
+    for (int i = 0; i < 8; ++i)
+    {
+        kill(pid[i], SIGKILL);
+    }
+}
