ba25df6f54a67ab6214c3bcbba8a837e63943b3c
pherna06 <pablohernandezaguado@gmail.com>
2020-11-10 01:10:07 +0100

    Fixed argument in not bash 'read'.

--- work/socket0.py
+++ work/socket0.py
@@ -14,7 +14,7 @@ for i in range(8):
         process(i)
         exit()
 
-os.system('read -n 1 -s -r -p "Press any key to continue"')
+os.system('read -p "Press Enter to continue"')
 
 for i in range(8):
     os.kill(pid[i], signal.SIGKILL)
