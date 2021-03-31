8610df2e1b0087f53510bc20616b87978a79f481
pherna06 <pablohernandezaguado@gmail.com>
2021-03-31 22:03:18 +0200

    Fixed tabs.

--- gym_envs/gymtest.py
+++ gym_envs/gymtest.py
@@ -14,41 +14,41 @@ def run_test(env, actions):
 		env : GYM Environment
 			The environment to be tested.
 	"""
-    env.reset()
-    sum_reward = 0
+	env.reset()
+	sum_reward = 0
 
-    for i in range(actions):
-        action = env.action_space.sample()
+	for i in range(actions):
+		action = env.action_space.sample()
 
-        if verbose:
-            print('action:', action)
+		if verbose:
+			print('action:', action)
 
-        state, reward, done, info = env.step(action)
-        sum_reward += reward
+		state, reward, done, info = env.step(action)
+		sum_reward += reward
 
-        if verbose:
-            env.render()
+		if verbose:
+			env.render()
 
-        if done:
-            if verbose:
-                print(f"done @ step {i}")
-            break
+		if done:
+			if verbose:
+				print(f"done @ step {i}")
+			break
     
-    if verbose:
-        print('cumulative reward', sum_reward)
+	if verbose:
+		print('cumulative reward', sum_reward)
 
-    return sum_reward
+	return sum_reward
 
 def get_parser():
-    # Parser description and creation.
-    desc = "A command interface that to test (not train) GYM environments."
-    parser = argparse.ArgumentParser(description = desc)
+	# Parser description and creation.
+	desc = "A command interface that to test (not train) GYM environments."
+	parser = argparse.ArgumentParser(description = desc)
 
-    env_help = "GYM environment name to be tested."
-    parser.add_argument(
-            'env', help=env_help,
-            type=str
-    )
+	env_help = "GYM environment name to be tested."
+	parser.add_argument(
+		'env', help=env_help,
+		type=str
+	)
 
 	act_help = "Number of actions to perform in test. "
 	act_help = "Set to {} by default.".format(DEF_ACTIONS)
@@ -58,19 +58,19 @@ def get_parser():
 		default = DEF_ACTIONS
 	)
 
-    return parser
+	return parser
 
 def main():
-    parser = get_parser()
-    args = parser.parse_args()
+	parser = get_parser()
+	args = parser.parse_args()
 
-    kwargs = {
-        'socket': 0,
-        'cores': [0],
-    }
+	kwargs = {
+		'socket': 0,
+		'cores': [0],
+	}
 
-    env = make(args.env, **kwargs)
-    run_test(env, args.actions)
+	env = make(args.env, **kwargs)
+	run_test(env, args.actions)
 
 if __name__ == "__main__":
-    main()
+	main()
