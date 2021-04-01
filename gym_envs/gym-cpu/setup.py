01067ee751822b7f4f41d31192a8d7761e6dc24f
pherna06 <pablohernandezaguado@gmail.com>
2021-04-01 04:01:25 +0200

    Indent to tabs.

--- gym_envs/gym-cpu/setup.py
+++ gym_envs/gym-cpu/setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(
-    name='gymcpu',
-    version='1.0.0',
-    install_requires = ['gym', 'cpufreq', 'pyRAPL']
+	name='gymcpu',
+	version='1.0.0',
+	install_requires = ['gym', 'cpufreq', 'pyRAPL']
 )
